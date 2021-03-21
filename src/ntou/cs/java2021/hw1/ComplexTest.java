8fc33beb7b363785d1cb223a9f91a06fad743508
chouguting <chouguting@gmail.com>
2021-03-21 20:59:01 +0800

    modify complex

--- src/ntou/cs/java2021/hw1/ComplexTest.java
+++ src/ntou/cs/java2021/hw1/ComplexTest.java
@@ -4,10 +4,12 @@ public class ComplexTest {
 
     public static void main(String[] args) {
         Complex complexA = new Complex(1.1, 2.2);
+        System.out.println("a = "+complexA);
         System.out.println("Conjugates of a = " + complexA.conjugates());
         System.out.printf("Absolute value of a = %.2f\n\n", complexA.absoluteValue());
 
         Complex complexB = new Complex(3.3, -4.4);
+        System.out.println("b = "+complexB);
         System.out.println("Conjugates of b = " + complexB.conjugates());
         System.out.printf("Absolute value of b = %.2f\n\n", complexB.absoluteValue());
 
@@ -18,10 +20,12 @@ public class ComplexTest {
 
 
         Complex complexC = new Complex();
+        System.out.println("c = "+complexC);
         System.out.println("Conjugates of c = " + complexC.conjugates());
         System.out.printf("Absolute value of c = %.2f\n\n", complexC.absoluteValue());
 
         Complex complexD = new Complex();
+        System.out.println("d = "+complexD);
         System.out.println("Conjugates of d = " + complexD.conjugates());
         System.out.printf("Absolute value of d = %.2f\n\n", complexD.absoluteValue());
 
